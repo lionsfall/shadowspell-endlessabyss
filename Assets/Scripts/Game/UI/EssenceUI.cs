59be35df343045d71287e66406f5b893203eacd6
Doğa Can Kılıç <dogabeey@gmail.com>
2024-08-04 17:13:21 +0300

    essence remaining life ui logic

--- Assets/Scripts/Game/UI/EssenceUI.cs
+++ Assets/Scripts/Game/UI/EssenceUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ namespace Dogabeey
     public class EssenceUI : UIElement
     {
         public List<Image> essenceImages;
+        public List<TMP_Text> essenceLifeTexts;
 
         internal Player player;
 
@@ -33,10 +35,12 @@ namespace Dogabeey
                 {
                     essenceImages[i].gameObject.SetActive(true);
                     essenceImages[i].sprite = player.essences[i].essence.essenceIcon;
+                    essenceLifeTexts[i].text = player.essences[i].remainingLifeSpan.ToString();
                 }
                 else
                 {
                     essenceImages[i].gameObject.SetActive(false);
+                    essenceLifeTexts[i].text = "";
                 }
             }
         }
