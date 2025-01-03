06e2b4dc51263b7568f18fdaea185015b88aef36
Doğa Can Kılıç <dogabeey@gmail.com>
2025-01-03 19:16:44 +0300

    [stat ui] implemented stats ui

--- Assets/Scripts/Game/PlayerStatModifiers/StatsUI.cs
+++ Assets/Scripts/Game/PlayerStatModifiers/StatsUI.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using TMPro; 
 
-public class StatsUI : MonoBehaviour
+namespace Dogabeey
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class StatsUI : UIElement
     {
-        
-    }
+        internal Player playerRef;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public TMP_Text speedStat;
+        public TMP_Text damageStat;
+        public TMP_Text attackRateStat;
+        public TMP_Text projectileSpeedStat;
+
+        public override string FireEvent => "STAT_CHANGED";
+
+        private void Start()
+        {
+            playerRef = Player.Instance ;
+        }
+
+        private void Update()
+        {
+            if(playerRef == null)
+            {
+                if(Player.Instance != null)
+                {
+                    playerRef = Player.Instance;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            DrawUI();
+        }
+
+        public override void DrawUI()
+        {
+            if (speedStat) speedStat.text = playerRef.Speed.ToString();
+            if (damageStat) damageStat.text = playerRef.Damage.ToString();
+            if (attackRateStat) attackRateStat.text = playerRef.AttackRate.ToString();
+            if (projectileSpeedStat) projectileSpeedStat.text = playerRef.ProjectileSpeed.ToString();
+        }
     }
 }
+
